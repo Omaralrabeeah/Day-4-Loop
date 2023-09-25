1380be1e54563c322e8dc0d07037dc9787f756bc
Omaralrabeeah <omar.alrabeeah@gmail.com>
2023-09-25 21:57:10 +0300

    done

---
+++ Task1.js
@@ -0,0 +1,14 @@
+// Question 1: Write a for loop in JavaScript that prints the numbers from 1 to 10.
+let u = 1;
+let y = 11;
+while (y > u) {
+  console.log(u);
+  u++;
+}
+// Question 2: Write a for loop in JavaScript that prints the even numbers from 2 to 20.
+for (let n = 2; n <= 20; n++) {
+  console.log(n);
+}
+// Question 3: Write a while loop in JavaScript that calculates the sum of all integers from 1 to 100.
+
+// Question 4 🌶️🌶️🌶️: Write a for loop in JavaScript that iterates through an array of numbers and calculates their sum.
